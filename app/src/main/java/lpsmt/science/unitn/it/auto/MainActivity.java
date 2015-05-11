58b37234c9178133c1bda85b8c4f254a11f7b654
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-05-11 10:34:16 +0200

    fix broken style

--- app/src/main/java/lpsmt/science/unitn/it/auto/MainActivity.java
+++ app/src/main/java/lpsmt/science/unitn/it/auto/MainActivity.java
@@ -1,10 +1,10 @@
 package lpsmt.science.unitn.it.auto;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
