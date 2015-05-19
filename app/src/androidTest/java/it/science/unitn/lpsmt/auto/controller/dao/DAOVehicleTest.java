6aa4d0cda9def43fc93142be957f6c9ece3f4fb0
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-05-19 13:36:09 +0200

    separated Vehicle model test to DAOVehicleTest

--- app/src/androidTest/java/it/science/unitn/lpsmt/auto/controller/dao/DAOVehicleTest.java
+++ app/src/androidTest/java/it/science/unitn/lpsmt/auto/controller/dao/DAOVehicleTest.java
@@ -2,11 +2,7 @@ package it.science.unitn.lpsmt.auto.controller.dao;
 
 import android.test.AndroidTestCase;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Locale;
 
 import it.science.unitn.lpsmt.auto.model.Vehicle;
 import it.science.unitn.lpsmt.auto.model.util.Const;
@@ -38,57 +34,9 @@ public class DAOVehicleTest extends AndroidTestCase {
         super.tearDown();
     }
 
-//==================================================================================================
-//  TESTS
-//==================================================================================================
-    public void testVehicleCreation(){
-        Vehicle tmp = new Vehicle(name, plate, purchaseDate, id);
-        try{
-            tmp.setId(null);
-            boolean cond = tmp.getId() == Const.NO_DB_ID_SET;
-            assertTrue("passing null to setId() will be set to NO_DB_ID_SET", cond);
-        }catch (IllegalArgumentException ex){
-            fail("Expecting setId(null) will set id to NO_DB_ID_SET");
-        }
-
-        try{
-            tmp.setName(null);
-            fail("I can set null string name.");
-        }catch (IllegalArgumentException ex){}
-
-        try{
-            tmp.setName("");
-            fail("I can set empty name.");
-        }catch (IllegalArgumentException ex){}
-
-        try{
-            tmp.setPlate(null);
-            fail("I can set null string as plate.");
-        }catch (IllegalArgumentException ex){}
-
-        try{
-            tmp.setPlate("");
-            fail("I can set empty plate.");
-        }catch (IllegalArgumentException ex){}
-
-        try{
-            tmp.setPurchaseDate(null);
-            assertTrue("I can set null as purchase date.", tmp.getPurchaseDate() == null);
-
-            Date d = getPastDate();
-            tmp.setPurchaseDate(d);
-            assertTrue("I can set a passed date as purchase date.", tmp.getPurchaseDate().equals(d));
-
-            tmp.setPurchaseDate(getTomorrowDate());
-            fail("I can set future date for purchase");
-        }catch (IllegalArgumentException ex){}
-
-        //TODO test cost
-    }
-
     public void testIODB(){
         // test the storing procedure.
-        Vehicle toStore = new Vehicle(name, plate, purchaseDate, id);
+        Vehicle toStore = new Vehicle("someName", "XXX123", new Date(), Const.NO_DB_ID_SET);
         Long idFromDB = this.daoVehicle.save(toStore);
         assertTrue("Id from DB must be different from NO_DB_ID_SET", !idFromDB.equals(Const.NO_DB_ID_SET));
 
@@ -102,24 +50,5 @@ public class DAOVehicleTest extends AndroidTestCase {
         Vehicle fromDb = this.daoVehicle.get(idFromDB);
         assertTrue("Vehicle from DB must be not null.", fromDb != null);
         assertTrue("Actual and stored Vehicle must be the same", toStore.equals(fromDb));
-    }
-
-
-//==================================================================================================
-//  METHOD
-//==================================================================================================
-    private Date getTomorrowDate(){
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.DAY_OF_YEAR, 1);
-        return c.getTime();
-    }
-
-    private Date getPastDate(){
-        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-        try {
-            return s.parse("1999-12-01");
-        } catch (ParseException e) {
-            return null;
-        }
     }
 }
---
+++ app/src/androidTest/java/it/science/unitn/lpsmt/auto/model/VehicleTest.java
@@ -0,0 +1,81 @@
+package it.science.unitn.lpsmt.auto.model;
+
+import junit.framework.TestCase;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import it.science.unitn.lpsmt.auto.model.util.Const;
+
+/**
+ * TODO add doc
+ */
+public class VehicleTest extends TestCase {
+
+    // TODO add description
+    public void testVehicleCreation(){
+        Vehicle tmp = new Vehicle("someName", "XXX123", new Date(), Const.NO_DB_ID_SET);
+        try{
+            tmp.setId(null);
+            boolean cond = tmp.getId() == Const.NO_DB_ID_SET;
+            assertTrue("passing null to setId() will be set to NO_DB_ID_SET", cond);
+        }catch (IllegalArgumentException ex){
+            fail("Expecting setId(null) will set id to NO_DB_ID_SET");
+        }
+
+        try{
+            tmp.setName(null);
+            fail("I can set null string name.");
+        }catch (IllegalArgumentException ex){}
+
+        try{
+            tmp.setName("");
+            fail("I can set empty name.");
+        }catch (IllegalArgumentException ex){}
+
+        try{
+            tmp.setPlate(null);
+            fail("I can set null string as plate.");
+        }catch (IllegalArgumentException ex){}
+
+        try{
+            tmp.setPlate("");
+            fail("I can set empty plate.");
+        }catch (IllegalArgumentException ex){}
+
+        try{
+            tmp.setPurchaseDate(null);
+            assertTrue("I can set null as purchase date.", tmp.getPurchaseDate() == null);
+
+            Date d = getPastDate();
+            tmp.setPurchaseDate(d);
+            assertTrue("I can set a passed date as purchase date.", tmp.getPurchaseDate().equals(d));
+
+            tmp.setPurchaseDate(getTomorrowDate());
+            fail("I can set future date for purchase");
+        }catch (IllegalArgumentException ex){}
+
+        //TODO test cost
+    }
+
+//==================================================================================================
+//  METHOD
+//==================================================================================================
+    private Date getTomorrowDate(){
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DAY_OF_YEAR, 1);
+        return c.getTime();
+    }
+
+    private Date getPastDate(){
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        try {
+            return s.parse("1999-12-01");
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+}
