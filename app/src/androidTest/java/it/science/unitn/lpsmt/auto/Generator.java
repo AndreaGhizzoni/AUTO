6e13527d6dcdeae46bcedc61e7f645098db39e02
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-05-22 15:26:04 +0200

    implements DAOCostTest

--- app/src/androidTest/java/it/science/unitn/lpsmt/auto/Generator.java
+++ app/src/androidTest/java/it/science/unitn/lpsmt/auto/Generator.java
@@ -1,10 +1,17 @@
 package it.science.unitn.lpsmt.auto;
 
+import android.location.Location;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+
+import it.science.unitn.lpsmt.auto.model.Maintenance;
+import it.science.unitn.lpsmt.auto.model.Place;
+import it.science.unitn.lpsmt.auto.model.Refuel;
+import it.science.unitn.lpsmt.auto.model.util.Const;
 
 public final class Generator {
     private Generator(){}
@@ -25,5 +32,22 @@ public final class Generator {
         } catch (ParseException e) {
             return null;
         }
+    }
+
+    public static Refuel getRefuelInstance(){
+        Location l = new Location("db");
+        l.setLongitude(10);
+        l.setLatitude(10);
+        Place p = new Place(Const.NO_DB_ID_SET, "address", l);
+        return new Refuel(Const.NO_DB_ID_SET, 10f, "notes", 1.4f, new Date(), 1000, p);
+    }
+
+    public static Maintenance getMaintenanceInstance(){
+        Location l = new Location("db");
+        l.setLongitude(10);
+        l.setLatitude(10);
+        Place p = new Place(Const.NO_DB_ID_SET, "address", l);
+        return new Maintenance(Const.NO_DB_ID_SET, 100f, "notes", "name",
+                Maintenance.Type.EXTRAORDINARY, p, 10);
     }
 }
