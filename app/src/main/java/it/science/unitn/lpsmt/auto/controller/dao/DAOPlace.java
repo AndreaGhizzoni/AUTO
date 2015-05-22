b76f5440d1eedaea3de3c9095b74dab711e2edac
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-05-22 11:39:56 +0200

    implemented DAOPLace

--- app/src/main/java/it/science/unitn/lpsmt/auto/controller/dao/DAOPlace.java
+++ app/src/main/java/it/science/unitn/lpsmt/auto/controller/dao/DAOPlace.java
@@ -1,11 +1,18 @@
 package it.science.unitn.lpsmt.auto.controller.dao;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.science.unitn.lpsmt.auto.controller.PlaceDAO;
+import it.science.unitn.lpsmt.auto.controller.util.Converter;
 import it.science.unitn.lpsmt.auto.model.Place;
+import it.science.unitn.lpsmt.auto.model.util.Const;
 
 /**
  * TODO add doc
@@ -18,34 +25,98 @@ public class DAOPlace implements PlaceDAO {
         db = PersistenceDAO.getInstance().getWritableDatabase();
     }
 
+    public DAOPlace( Context testContext ){
+        db = new PersistenceDAO(testContext).getWritableDatabase();
+    }
+
     @Override
     public Long save(Place p) {
-        return null;
+        if( p == null )
+            return Const.NO_DB_ID_SET;
+
+        Long id = Const.NO_DB_ID_SET;
+        db.beginTransaction();
+        try{
+            ContentValues cv = Converter.placeToContentValues(p);
+            id = db.insert( Place.SQLData.TABLE_NAME, null, cv );
+            db.setTransactionSuccessful();
+        }catch (Throwable t){
+            Log.e(DAOPlace.class.getSimpleName(), t.getMessage());
+        }finally {
+            db.endTransaction();
+        }
+
+        return id;
     }
 
     @Override
     public Place get(Long id) {
-        return null;
+        if( id == null || id.equals(Const.NO_DB_ID_SET) )
+            return null;
+
+        Cursor c = db.query(              // select from
+            Place.SQLData.TABLE_NAME,
+            Place.SQLData.ALL_COLUMNS,    // where
+            Place.SQLData.ID+" = ?",
+            new String[]{id.toString()},
+            null, null, null
+        );
+        c.moveToFirst();
+        Place p = Converter.cursorToPlace(c);
+        c.close();
+        return p;
     }
 
     @Override
     public boolean exists(Long id) {
-        return false;
-    }
-
-    @Override
-    public void delete(Place p) {
-
+        return !(id == null || id.equals(Const.NO_DB_ID_SET)) && get(id) != null;
     }
 
     @Override
     public void delete(Long id) {
+        if( id == null || id.equals(Const.NO_DB_ID_SET) )
+            return;
 
+        db.beginTransaction();
+        try{
+            db.delete(
+                Place.SQLData.TABLE_NAME,
+                Place.SQLData.ID+" = ?",
+                new String[]{id.toString()}
+            );
+        }catch (Throwable t){
+            Log.e(DAOPlace.class.getSimpleName(), t.getMessage());
+        }finally {
+            db.endTransaction();
+        }
+    }
+
+    @Override
+    public void delete(Place p) {
+        if( p == null )
+            return;
+        delete(p.getId());
     }
 
     @Override
     public List<Place> getAll() {
-        return null;
+        ArrayList<Place> list = new ArrayList<>();
+        Cursor c = db.query(
+            Place.SQLData.TABLE_NAME,
+            Place.SQLData.ALL_COLUMNS,
+            null, null, null, null, null
+        );
+
+        c.moveToFirst();
+        Place tmp;
+        while( !c.isAfterLast() ){
+            tmp = Converter.cursorToPlace(c);
+            list.add(tmp);
+            c.moveToNext();
+        }
+        c.close();
+
+        return list;
     }
 
     @Override
