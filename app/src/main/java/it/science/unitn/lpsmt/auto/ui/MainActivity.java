bd10c13a8ecced8fcc3a8d3a052b8f3ab161470f
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-05-26 10:41:32 +0200

    added basic MainActivity layout and stuff

--- app/src/main/java/it/science/unitn/lpsmt/auto/ui/MainActivity.java
+++ app/src/main/java/it/science/unitn/lpsmt/auto/ui/MainActivity.java
@@ -3,23 +3,59 @@ package it.science.unitn.lpsmt.auto.ui;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import lpsmt.science.unitn.it.auto.R;
 
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
     private static MainActivity instance;
+
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         instance = this;
+
+        // set up the drawer's list view with items and click listener
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ListView mDrawerList = (ListView) findViewById(R.id.drawer_list);
+        mDrawerList.setAdapter( new ArrayAdapter<>(
+                this,
+                R.layout.adapter_drawer_list,
+                getResources().getStringArray(R.array.drawer_items)
+            )
+        );
+        // TODO mDrawerList set OnClickActionListener
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ){
+            public void onDrawerClosed(View view) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
 
     public static Context getAppContext(){
         return instance.getApplicationContext();
-
     }
 }
