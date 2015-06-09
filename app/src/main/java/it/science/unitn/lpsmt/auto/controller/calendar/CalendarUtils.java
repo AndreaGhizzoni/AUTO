9a4518277b8c313d005d2277a3cb20fe1b7b61dd
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-06-09 14:58:45 +0200

    test the methods to put raw event without starting new activity

--- app/src/main/java/it/science/unitn/lpsmt/auto/controller/calendar/CalendarUtils.java
+++ app/src/main/java/it/science/unitn/lpsmt/auto/controller/calendar/CalendarUtils.java
@@ -1,6 +1,7 @@
 package it.science.unitn.lpsmt.auto.controller.calendar;
 
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -10,6 +11,7 @@ import android.text.format.DateUtils;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 import it.science.unitn.lpsmt.auto.ui.MainActivity;
 
@@ -20,8 +22,10 @@ import static android.provider.CalendarContract.Events.DESCRIPTION;
 import static android.provider.CalendarContract.Events.DTEND;
 import static android.provider.CalendarContract.Events.DTSTART;
 import static android.provider.CalendarContract.Events.EVENT_LOCATION;
+import static android.provider.CalendarContract.Events.EVENT_TIMEZONE;
 import static android.provider.CalendarContract.Events.RDATE;
 import static android.provider.CalendarContract.Events.TITLE;
+import static android.provider.CalendarContract.Events.HAS_ALARM;
 
 /**
  * TODO add doc
@@ -29,9 +33,13 @@ import static android.provider.CalendarContract.Events.TITLE;
 public class CalendarUtils {
     private static CalendarUtils instance;
 
-    private static final String CALENDAR_EVENTS_URI = "content://com.android.calendar/events";
+    private static final Integer DEFAULT_CALENDAR_ID = 1;
     private Context context;
 
+    /**
+     * TODO add doc
+     * @return
+     */
     public static CalendarUtils getInstance(){
         if(instance == null)
             instance = new CalendarUtils();
@@ -58,15 +66,20 @@ public class CalendarUtils {
         return i;
     }
 
-    // https://goo.gl/C0hXD1
+    /**
+     * TODO add doc
+     * @param calID
+     * @return
+     */
     public CalendarUtils.Holder getEventDataById( Integer calID ){
+        // https://goo.gl/C0hXD1
         ContentResolver cr = this.context.getContentResolver();
         Cursor cursor = cr.query(
-            Uri.parse(CALENDAR_EVENTS_URI),
-            new String[]{CALENDAR_ID, TITLE, EVENT_LOCATION, DESCRIPTION, DTSTART, DTEND, RDATE},
-            CALENDAR_ID + " = ?",
-            new String[]{calID + ""},
-            null
+                CONTENT_URI,
+                new String[]{CALENDAR_ID, TITLE, EVENT_LOCATION, DESCRIPTION, DTSTART, DTEND, RDATE},
+                CALENDAR_ID + " = ?",
+                new String[]{calID + ""},
+                null
         );
 
         if( cursor.getCount() == 0 )
@@ -87,13 +100,37 @@ public class CalendarUtils {
         return h;
     }
 
+    public long putEvent( CalendarUtils.Holder data ){
+        ContentResolver cr = context.getContentResolver();
+        ContentValues cv = data.toContentValue();
+        cv.put(CALENDAR_ID, DEFAULT_CALENDAR_ID);
+
+        // Set Event in calendar.
+        Uri eventUri = cr.insert(CONTENT_URI, cv);
+        // Getting ID of event in Long.
+        return Long.parseLong(eventUri.getLastPathSegment());
+    }
+
+    public long putEventWithReminder( CalendarUtils.Holder data ){
+        long eventID = putEvent(data);
+        ContentResolver cr = context.getContentResolver();
+        // String to access default google calendar of device for reminder setting.
+        ContentValues rv = new ContentValues();
+        rv.put(CalendarContract.Reminders.EVENT_ID, eventID);
+        rv.put(CalendarContract.Reminders.MINUTES, (60*24)*2); // 2 days
+        rv.put(CalendarContract.Reminders.METHOD, CalendarContract.Reminders.METHOD_ALERT);
+        //Setting reminder in calendar on Event.
+        cr.insert(CalendarContract.Reminders.CONTENT_URI, rv);
+        return eventID;
+    }
+
 //==================================================================================================
 //  INNER CLASS
 //==================================================================================================
     /**
      * This class hold the data from reading an event from a calendar.
      */
-    public class Holder{
+    public static class Holder{
         public Integer calendarID;
         public String title;
         public String location;
@@ -101,5 +138,20 @@ public class CalendarUtils {
         public Date dStart;
         public Date dEnd;
         public Date rDate;
+        public boolean hasAlarm;
+
+        private ContentValues toContentValue(){
+            ContentValues cv = new ContentValues();
+            cv.put(TITLE, title);
+            cv.put(EVENT_LOCATION, location);
+            cv.put(DESCRIPTION, description);
+            cv.put(DTSTART, dStart.getTime());
+            cv.put(DTEND, dEnd.getTime());
+            if( rDate != null )
+                cv.put(RDATE, rDate.getTime());
+            cv.put(HAS_ALARM, hasAlarm ? 1 : 0);
+            cv.put(EVENT_TIMEZONE, TimeZone.getDefault().getDisplayName());
+            return cv;
+        }
     }
 }
