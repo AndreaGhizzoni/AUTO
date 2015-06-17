02423f6c4e9754717cc06e69c467e176c6c43672
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-06-17 17:02:46 +0200

    inner fragment maintenance.type.extraordinary now works with data picker

--- app/src/main/java/it/science/unitn/lpsmt/auto/ui/MaintenanceInsertion.java
+++ app/src/main/java/it/science/unitn/lpsmt/auto/ui/MaintenanceInsertion.java
@@ -1,15 +1,25 @@
 package it.science.unitn.lpsmt.auto.ui;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.List;
 
 import it.science.unitn.lpsmt.auto.controller.dao.DAOVehicle;
@@ -19,6 +29,7 @@ import lpsmt.science.unitn.it.auto.R;
 
 // TODO maybe implements method to save the app instance when is put onPause
 public class MaintenanceInsertion extends ActionBarActivity {
+    public static MaintenanceInsertion instance;
     private List<Vehicle> vehicleList;
     private Vehicle vehicleSelectedBySpinner;
     private Maintenance.Type maintenanceSelectedBySpinner;
@@ -33,6 +44,7 @@ public class MaintenanceInsertion extends ActionBarActivity {
 
         this.initSpinnerVehicleAssociated();
         this.initSpinnerTypeMaintenance();
+        instance = this;
     }
 
     private void initSpinnerVehicleAssociated(){
@@ -74,6 +86,22 @@ public class MaintenanceInsertion extends ActionBarActivity {
         maintenanceType.setAdapter(spinnerAdapter);
     }
 
+    private void selectMaintenanceFragment(){
+        Bundle args = new Bundle();
+        args.putString(MaintenanceTypeArgsFragment.TYPE, maintenanceSelectedBySpinner.toString());
+
+        Fragment f = new MaintenanceTypeArgsFragment();
+        f.setArguments(args);
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transaction.replace(R.id.maintenance_insertion_type_args, f).commit();
+    }
+
+    public void showDatePickerDialog(){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -95,6 +123,47 @@ public class MaintenanceInsertion extends ActionBarActivity {
 //==================================================================================================
 //  INNER CLASS
 //==================================================================================================
+    public static class MaintenanceTypeArgsFragment extends Fragment {
+        public static final String TYPE = "type";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            Bundle args = getArguments();
+            Maintenance.Type type = null;
+            if (args != null && args.containsKey(TYPE)) {
+                type = Maintenance.Type.valueOf((String) args.get(TYPE));
+            }
+
+            View v;
+            switch (type) {
+                case EXTRAORDINARY: {
+                    v = inflater.inflate(R.layout.activity_maintenance_inner_frag_type_extraordinary,
+                            container, false);
+                    v.findViewById(R.id.maintenance_insertion_inner_frag_extraordinary_date)
+                            .setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    MaintenanceInsertion.instance.showDatePickerDialog();
+                                }
+                            });
+                    break;
+                }
+                default: { // tax and ordinary
+                    v = inflater.inflate(R.layout.activity_maintenance_inner_frag_type_tax_ordinary,
+                            container, false);
+                    v.findViewById(R.id.btnAddCalendarEvent).setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    break;
+                }
+            }
+            return v;
+        }
+    }
+
+
     private class SpinnerVehicleSelection implements AdapterView.OnItemSelectedListener{
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
@@ -110,19 +179,50 @@ public class MaintenanceInsertion extends ActionBarActivity {
         public void onNothingSelected(AdapterView<?> adapterView) {}
     }
 
+
     private class SpinnerMaintenanceTypeSelection implements AdapterView.OnItemSelectedListener{
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
             if( pos == 0 ){
                 maintenanceSelectedBySpinner = null;
+                Toast.makeText(getApplicationContext(), "pos == 0", Toast.LENGTH_LONG).show();
+                findViewById(R.id.maintenance_insertion_type_args).setVisibility(View.INVISIBLE);
             }else{
-                maintenanceSelectedBySpinner = Maintenance.Type.valueOf(
-                        maintenanceType.getSelectedItem().toString());
-                Toast.makeText(getApplicationContext(), maintenanceSelectedBySpinner.toString(),
-                        Toast.LENGTH_LONG).show();
+                Maintenance.Type newT = Maintenance.Type.valueOf(maintenanceType.getSelectedItem().toString());
+                if( !newT.equals(maintenanceSelectedBySpinner)) {
+                    maintenanceSelectedBySpinner = newT;
+                            Toast.makeText(getApplicationContext(), maintenanceSelectedBySpinner.toString(),
+                                    Toast.LENGTH_LONG).show();
+                    selectMaintenanceFragment();
+                    findViewById(R.id.maintenance_insertion_type_args).setVisibility(View.VISIBLE);
+                }
             }
         }
         @Override
         public void onNothingSelected(AdapterView<?> adapterView) {}
+    }
+
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            String format = "%d/%d/%d";
+            String date = String.format(format, day, month+1, year);
+            EditText edt = (EditText) getActivity()
+                    .findViewById(R.id.maintenance_insertion_inner_frag_extraordinary_date);
+            edt.setText(date);
+        }
     }
 }
