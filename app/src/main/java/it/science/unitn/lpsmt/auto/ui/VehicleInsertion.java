5b8e0615f4ef8b43f3c3f05c3f6a414a3189078c
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-06-20 16:27:14 +0200

    fix data picker

--- app/src/main/java/it/science/unitn/lpsmt/auto/ui/VehicleInsertion.java
+++ app/src/main/java/it/science/unitn/lpsmt/auto/ui/VehicleInsertion.java
@@ -1,6 +1,8 @@
 package it.science.unitn.lpsmt.auto.ui;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
@@ -8,9 +10,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import lpsmt.science.unitn.it.auto.R;
 
@@ -51,7 +56,7 @@ public class VehicleInsertion extends ActionBarActivity {
     }
 
     private void showDatePickerDialog(){
-        DialogFragment newFragment = new MaintenanceInsertion.DatePickerFragment();
+        DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 //==================================================================================================
@@ -83,5 +88,30 @@ public class VehicleInsertion extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+//==================================================================================================
+//  INNER CLASS
+//==================================================================================================
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            String format = "%d/%d/%d";
+            String date = String.format(format, day, month+1, year);
+            EditText edt = (EditText) getActivity().findViewById(R.id.vehicle_insertion_purchase_data);
+            edt.setText(date);
+        }
     }
 }
