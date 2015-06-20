5da677dcc2c6302bfc6049a0a1b604ac53098df7
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-06-20 16:05:20 +0200

    added basic done action button

--- app/src/main/java/it/science/unitn/lpsmt/auto/ui/VehicleInsertion.java
+++ app/src/main/java/it/science/unitn/lpsmt/auto/ui/VehicleInsertion.java
@@ -1,5 +1,6 @@
 package it.science.unitn.lpsmt.auto.ui;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import lpsmt.science.unitn.it.auto.R;
 
@@ -64,12 +66,20 @@ public class VehicleInsertion extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_vehicle_insertion, menu);
+        getMenuInflater().inflate(R.menu.menu_action_done, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.done) {
+            // check and save
+            Toast.makeText(getApplicationContext(), "Done button", Toast.LENGTH_SHORT).show();
+            setResult(Activity.RESULT_OK);
+            finish();
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
