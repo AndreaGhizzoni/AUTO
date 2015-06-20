5aff524c74bc77737ce6513dbac4cba731cda8b8
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-06-20 16:02:33 +0200

    refactoring

--- app/src/main/java/it/science/unitn/lpsmt/auto/ui/MaintenanceInsertion.java
+++ app/src/main/java/it/science/unitn/lpsmt/auto/ui/MaintenanceInsertion.java
@@ -340,7 +340,7 @@ public class MaintenanceInsertion extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_maintenance_insertion, menu);
+        getMenuInflater().inflate(R.menu.menu_action_done, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
