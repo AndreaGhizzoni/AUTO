4bbe578cce317b01610958570725e4a798af1196
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-06-20 16:00:45 +0200

    first implementation of vehicle insertion

--- app/src/main/java/it/science/unitn/lpsmt/auto/ui/VehicleInsertion.java
+++ app/src/main/java/it/science/unitn/lpsmt/auto/ui/VehicleInsertion.java
@@ -1,19 +1,64 @@
 package it.science.unitn.lpsmt.auto.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import lpsmt.science.unitn.it.auto.R;
 
 // TODO maybe implements method to save the app instance when is put onPause
 public class VehicleInsertion extends ActionBarActivity {
+    // gui components
+    private EditText editName;
+    private EditText editPlate;
+    private Spinner spinnerFuel;
+    private EditText editPurchaseDate;
 
+//==================================================================================================
+//  METHOD
+//==================================================================================================
+    private void initComponents(){
+        this.editName = (EditText)findViewById(R.id.vehicle_insertion_name);
+        this.editPlate = (EditText)findViewById(R.id.vehicle_insertion_plate);
+
+        this.spinnerFuel = (Spinner)findViewById(R.id.vehicle_insertion_spinner_fuel);
+        ArrayAdapter<CharSequence> spinnerAdapter = new ArrayAdapter<>(
+            getApplicationContext(),R.layout.spinner_item
+        );
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAdapter.add("select a fuel");
+        for( String s : getResources().getStringArray(R.array.fuel) )
+            spinnerAdapter.add(s);
+        this.spinnerFuel.setAdapter(spinnerAdapter);
+
+        this.editPurchaseDate = (EditText)findViewById(R.id.vehicle_insertion_purchase_data);
+        this.editPurchaseDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) showDatePickerDialog();
+            }
+        });
+    }
+
+    private void showDatePickerDialog(){
+        DialogFragment newFragment = new MaintenanceInsertion.DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+//==================================================================================================
+//  OVERRIDE
+//==================================================================================================
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_insertion);
+
+        this.initComponents();
     }
 
     @Override
@@ -25,16 +70,6 @@ public class VehicleInsertion extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
         return super.onOptionsItemSelected(item);
     }
 }
