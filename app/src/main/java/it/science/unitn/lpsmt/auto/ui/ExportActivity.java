544f80e92266803396eabd0d7d7654216b3a3e9b
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-06-23 17:59:50 +0200

    create empty activity for exporting data

---
+++ app/src/main/java/it/science/unitn/lpsmt/auto/ui/ExportActivity.java
@@ -0,0 +1,15 @@
+package it.science.unitn.lpsmt.auto.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import lpsmt.science.unitn.it.auto.R;
+
+public class ExportActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_export);
+    }
+}
